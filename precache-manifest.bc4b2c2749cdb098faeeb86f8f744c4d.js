5266e2284c077ebc10c7497d6a5e46ec349d0ccd
Dhruv Patel <druv5319@gmail.com>
2020-08-08 00:51:50 -0400

    Updates

---
+++ precache-manifest.bc4b2c2749cdb098faeeb86f8f744c4d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2c2a9bcb27e1cc1cc775874cc02b42",
+    "url": "/sneaks-app.com/index.html"
+  },
+  {
+    "revision": "639f04efd2431aa6cc20",
+    "url": "/sneaks-app.com/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e4009cc5376a367c7a39",
+    "url": "/sneaks-app.com/static/css/main.5099807b.chunk.css"
+  },
+  {
+    "revision": "639f04efd2431aa6cc20",
+    "url": "/sneaks-app.com/static/js/2.eb7c0d58.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/sneaks-app.com/static/js/2.eb7c0d58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4009cc5376a367c7a39",
+    "url": "/sneaks-app.com/static/js/main.4f2c5f7d.chunk.js"
+  },
+  {
+    "revision": "d25fc293978682f04757",
+    "url": "/sneaks-app.com/static/js/runtime-main.57ec6d47.js"
+  },
+  {
+    "revision": "b91240ab2303cb22a3f7d700fcdebb3d",
+    "url": "/sneaks-app.com/static/media/Sneaks_Logo.b91240ab.png"
+  },
+  {
+    "revision": "3d419a668b06f6a2202ac986d33ed620",
+    "url": "/sneaks-app.com/static/media/background1.3d419a66.png"
+  },
+  {
+    "revision": "ce23488a4750e4287d124047576a3ab0",
+    "url": "/sneaks-app.com/static/media/flightclub.ce23488a.png"
+  },
+  {
+    "revision": "00eed65cd15a78db65773f850849522e",
+    "url": "/sneaks-app.com/static/media/goat.00eed65c.png"
+  },
+  {
+    "revision": "1add91617802dde01f18c91c790e197d",
+    "url": "/sneaks-app.com/static/media/stadiumgoods.1add9161.png"
+  },
+  {
+    "revision": "d8e1c1bae0d3e2c1ac7322de27376078",
+    "url": "/sneaks-app.com/static/media/stockx.d8e1c1ba.png"
+  }
+]);
