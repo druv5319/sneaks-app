c6f3c0fe476f6a0151ae13fbfd56dbce8869b6e9
Dhruv Patel <druv5319@gmail.com>
2020-08-15 19:32:03 -0400

    Changed search icon to be compatible with mobile devices

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
+import searchIcon from '../images/search.png'
 const SearchBar = (props) => {
     let history = useHistory();
     const [searchValue, setSearchValue] = useState("");
@@ -22,11 +22,9 @@ const SearchBar = (props) => {
                   <div class="input-field">
                     <input  value={searchValue} onChange={handleSearchInputChanges} type="text" placeholder = "Search Shoe"/>
                     <div class="icon-wrap">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={callSearchFunction} type="submit" value="SEARCH" >
-                        <path
-                          d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
-                        </path>
-                      </svg>
+                      <img src={searchIcon} width="24" height="24"  onClick={callSearchFunction} type="submit" value="SEARCH" >
+
+                      </img>
                     </div>
                   </div>
                 </div>
